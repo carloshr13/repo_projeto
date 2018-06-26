9b54840fad02518dcbe2907d922583d1080316ef
Carlos <carloshr13@hotmail.com>
2018-06-25 22:11:35 -0300

    primeiro commit

---
+++ App.js
@@ -0,0 +1,68 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Platform
+} from 'react-native';
+
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}> Olá mundo!</Text>
+        </View>
+          <ScrollView contentContainerStyle={styles.repoList}>
+            <View style={styles.repo} />
+            <View style={styles.repo} />
+            <View style={styles.repo} />
+            <View style={styles.repo} />
+            <View style={styles.repo} />
+            <View style={styles.repo} />
+            <View style={styles.repo} />
+          </ScrollView>
+       </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#333',
+  },
+
+  header: {
+    height: (Platform.OS === 'ios') ? 70 : 50,
+    paddingTop: (Platform.OS === 'ios') ? 20 : 0,
+    backgroundColor: '#FFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  headerText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+
+  repoList: {
+    padding: 20,
+  },
+
+  repo: {
+    padding: 20,
+    backgroundColor: '#FFF',
+    height: 120,
+    marginBottom: 20,
+    borderRadius: 5,
+  },
+});
